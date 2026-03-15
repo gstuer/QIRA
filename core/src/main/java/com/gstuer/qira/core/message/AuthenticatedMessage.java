cf98ce6cd42b43cacedfedd2a5dc5502c4e34a8c
Moritz Gstuer <moritz@gstuer.com>
2026-03-15 15:14:09 +0100

    Fix missing verify method in auth. message

--- core/src/main/java/com/gstuer/qira/core/message/AuthenticatedMessage.java
+++ core/src/main/java/com/gstuer/qira/core/message/AuthenticatedMessage.java
@@ -1,8 +1,11 @@
 package com.gstuer.qira.core.message;
 
 import com.gstuer.qira.core.cryptography.signature.DigitalSignature;
+import com.gstuer.qira.core.cryptography.signature.Verifier;
 
 import java.net.InetAddress;
+import java.security.InvalidKeyException;
+import java.security.SignatureException;
 import java.util.Objects;
 
 public class AuthenticatedMessage extends Message<Message<?>> {
@@ -16,6 +19,10 @@ public class AuthenticatedMessage extends Message<Message<?>> {
     protected AuthenticatedMessage(InetAddress destination, Message<?> payload, DigitalSignature signature) {
         super(destination, payload);
         this.signature = Objects.requireNonNull(signature);
+    }
+
+    public boolean verify(Verifier<?> verifier) throws SignatureException, InvalidKeyException {
+        return verifier.verify(this.getPayload().getSigningData(), this.signature);
     }
 
     public DigitalSignature getSignature() {
